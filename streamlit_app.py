04706dab51ddfb652bc82f03690af7719a2c225a
ZAC DEMOS <zachariah.demos@gmail.com>
2023-03-28 13:10:05 +1100

    2

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,28 +1,6 @@
 import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 streamlit.header('Breakfast Menu')
 streamlit.text('Omega 3 & Bluberrt Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
